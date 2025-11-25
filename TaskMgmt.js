53f0f15dafc7166704c012a39433666c6dcf21b9
JerodBMC <jerod@bettermindcare.com>
2025-11-24 21:05:58 -0600

    Completed DTL Assignment.

--- TaskMgmt.js
+++ TaskMgmt.js
@@ -3,20 +3,22 @@ let taskDurations = [];
 let report = [];
 
 
-taskNames.push("Review Pull Requests");
-taskDurations.push("1.5 hrs");
-
-taskNames.push("Write Documentation");
-taskDurations.push("4 hrs");
 
 taskNames.push("Fix Database Bug");
 taskDurations.push(".75 hrs");
+
+taskNames.push("Review Pull Requests");
+taskDurations.push("1.5 hrs");
 
 taskNames.push("Client Meeting Prep");
 taskDurations.push("2 hrs");
 
 taskNames.push("Deploy New Feature");
 taskDurations.push("3.5 hours");
+
+taskNames.push("Write Documentation");
+taskDurations.push("4 hrs");
+
 
 console.table(taskNames);
 console.table(taskDurations);
